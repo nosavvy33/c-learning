0142a224361542c64614557636a2ea1196433910
nosavvy33 <bleonp1997@gmail.com>
2018-02-16 18:12:04 -0500

    third load

--- directives.c
+++ directives.c
@@ -3,8 +3,25 @@
                         #define MESSAGE "Hello from the header section\n"
                 #endif
 
-                 int main(){ 
+		#if !defined(GREET)
+			#define GREET "Hello, you almost forgot the GREET string"
+		#endif 
+
+		#if !defined(COMPARISON)
+			#define MAX(x,y) \
+			((x) > (y) ? (x) : (y))
+		#endif
+
+                int main(){ 
+		int x,y;
                 printf(MESSAGE);
-                return 0;
+		printf("Date: %s\n",__DATE__);
+		printf("%s\n",GREET);
+		printf("Enter x value \n");
+		scanf("%d",&x);
+		printf("Enter y value \n");
+		scanf("%d",&y);
+		printf("El nro mayor entre x e y es : %d\n",MAX(x,y));                
+return 0;
                 }
 
