1d598af4c04589b31d822746ccdeccc1cb6406d6
nosavvy33 <bleonp1997@gmail.com>
2018-02-16 17:30:35 -0500

    second load

---
+++ directives.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+                #ifndef MESSAGE
+                        #define MESSAGE "Hello from the header section\n"
+                #endif
+
+                 int main(){ 
+                printf(MESSAGE);
+                return 0;
+                }
+
