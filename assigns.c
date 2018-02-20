7ddeb29ee10e47fc25439979057ca4291b6e1c99
nosavvy33 <bleonp1997@gmail.com>
2018-02-20 11:18:45 -0500

    last operator

---
+++ assigns.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(){
+int x,y,z;
+printf("Value for x");
+scanf("%d",&x);
+printf("Value for y");
+scanf("%d",&y);
+printf("Value for z");
+scanf("%d",&z);
+printf("Value of x = %d\nValue of y = %d\nValue of z = %d\n",x,y,z);
+
+
+
+return 0;
+}
